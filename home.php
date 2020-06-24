f96024c411fd9809d96d31b91a9412c7ec8138a0
isienmai <ben.strutt@hotmail.co.uk>
2020-06-24 13:37:55 +0100

    Initial commit of dud homepage. There's nothing here...YET

---
+++ home.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+      <title>Oh my god I have a Website</title>
+  </head>
+  <body>
+      <p>This was a terrible idea I don't even know what an internet IS</p>
+  </body>
+</html>
