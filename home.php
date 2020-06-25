ecbc6c2ff12f1bfe7aa6462974138342665e9750
isienmai <ben.strutt@hotmail.co.uk>
2020-06-25 12:27:45 +0100

    Started Moving the old project deets across.

--- home.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-      <title>Oh my god I have a Website</title>
-  </head>
-  <body>
-      <p>This was a terrible idea I don't even know what an internet IS</p>
-  </body>
-</html>
