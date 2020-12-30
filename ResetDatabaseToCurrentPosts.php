6bf3690f40a727f8c05e3cb39de7cfb80bf9ed31
isienmai <ben.strutt@hotmail.co.uk>
2020-12-30 15:47:36 +0000

    Remove old dummy pages and added new test dummy pages.

---
+++ ResetDatabaseToCurrentPosts.php
@@ -0,0 +1,14 @@
+<?php
+  include_once($_SERVER['DOCUMENT_ROOT'] . "/DatabaseFunctions.php");
+  // Wipe database data (Leave the structure)
+  BS_QueryDatabase("TRUNCATE TABLE `Posts`");
+  BS_QueryDatabase("TRUNCATE TABLE `PostTags`");
+  BS_QueryDatabase("TRUNCATE TABLE `Tags`");
+
+  // Grab all Post Folders
+  BS_QueryDatabase("INSERT INTO `Posts` (`ID`, `Name`, `CreationDate`) VALUES (NULL, 'TESTIG', '2020-12-30')");
+  BS_QueryDatabase("INSERT INTO `Tags` (`ID`, `Name`, `IsCategory`) VALUES (NULL, 'TestCategory', 1)");
+  BS_QueryDatabase("INSERT INTO `Tags` (`ID`, `Name`, `IsCategory`) VALUES (NULL, 'TestTag', 0)");
+  BS_QueryDatabase("INSERT INTO `PostTags` (`ID`, `PostId`, `TagId`) VALUES (NULL, 1, 1)");
+  // For each one add the post then add its tags and other data from the csv
+?>
