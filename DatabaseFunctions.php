e1d575056f459f7db76cb7d77035575736126aa4
isienmai <ben.strutt@hotmail.co.uk>
2020-12-30 16:08:35 +0000

    Ditch the nested includes completely

--- DatabaseFunctions.php
+++ DatabaseFunctions.php
@@ -71,4 +71,28 @@ function BS_ExecuteDatabaseQueryParameterised($dbConnection, $query, $parameters
 	$preparedStatement->close();
 	return $query_result;
 }
+
+function BS_ResetDatabaseToCurrentPosts()
+{
+	BS_WipeDatabaseData();
+	BS_InsertAllPostsToDatabase();
+}
+
+function BS_WipeDatabaseData()
+{
+  // Wipe database data (Leave the structure)
+  BS_QueryDatabase("TRUNCATE TABLE `Posts`");
+  BS_QueryDatabase("TRUNCATE TABLE `PostTags`");
+  BS_QueryDatabase("TRUNCATE TABLE `Tags`");
+}
+
+function BS_InsertAllPostsToDatabase()
+{
+  // Grab all Post Folders
+  BS_QueryDatabase("INSERT INTO `Posts` (`ID`, `Name`, `CreationDate`) VALUES (NULL, 'TESTIG', '2020-12-30')");
+  BS_QueryDatabase("INSERT INTO `Tags` (`ID`, `Name`, `IsCategory`) VALUES (NULL, 'TestCategory', 1)");
+  BS_QueryDatabase("INSERT INTO `Tags` (`ID`, `Name`, `IsCategory`) VALUES (NULL, 'TestTag', 0)");
+  BS_QueryDatabase("INSERT INTO `PostTags` (`ID`, `PostId`, `TagId`) VALUES (NULL, 1, 1)");
+  // For each one add the post then add its tags and other data from the csv
+}
 ?>
