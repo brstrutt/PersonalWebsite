d6b5867826d3d0542d4e39fed6f3e3dcdd813bcb
isienmai <ben.strutt@hotmail.co.uk>
2021-02-07 13:02:43 +0000

    try setting the document root as it is used by the DatabaseFunctions

--- ResetDatabaseToCurrentPosts.php
+++ ResetDatabaseToCurrentPosts.php
@@ -1,7 +1,8 @@
 <?php
 echo "Include the database functions!";
+$_SERVER['DOCUMENT_ROOT'] = "/home/u296099994/public_html/dev";
 echo "HMMM" . $_SERVER['DOCUMENT_ROOT'] . "YES";
-include_once("/home/u296099994/public_html/dev/DatabaseFunctions.php");
+include_once($_SERVER['DOCUMENT_ROOT'] . "/DatabaseFunctions.php");
 echo "Start resetting the doongaloong!";
 BS_ResetDatabaseToCurrentPosts();
 echo "Done!";
