3fee9cd0e9f7f3c3e1a4a236bbb53184ff062bd5
isienmai <ben.strutt@hotmail.co.uk>
2021-02-07 12:05:32 +0000

    Modify a debug echo to confirm the latest code is in use

--- ResetDatabaseToCurrentPosts.php
+++ ResetDatabaseToCurrentPosts.php
@@ -1,7 +1,7 @@
 <?php
 echo "Include the database functions!";
 include_once($_SERVER['DOCUMENT_ROOT'] . "/DatabaseFunctions.php");
-echo "Start resetting the dingaling!";
+echo "Start resetting the doongaloong!";
 BS_ResetDatabaseToCurrentPosts();
 echo "Done!";
 ?>
