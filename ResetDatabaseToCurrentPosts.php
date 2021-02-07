ce0604324ef738e393a0966e69a4162afd5b5c62
isienmai <ben.strutt@hotmail.co.uk>
2021-02-07 13:05:25 +0000

    set DOCUMENT_ROOT so things can actually work

--- ResetDatabaseToCurrentPosts.php
+++ ResetDatabaseToCurrentPosts.php
@@ -1,6 +1,6 @@
 <?php
 echo "Include the database functions!";
-$_SERVER['DOCUMENT_ROOT'] = "/home/u296099994/public_html/dev";
+$_SERVER['DOCUMENT_ROOT'] = dirname(__FILE__);
 echo "HMMM" . $_SERVER['DOCUMENT_ROOT'] . "YES";
 include_once($_SERVER['DOCUMENT_ROOT'] . "/DatabaseFunctions.php");
 echo "Start resetting the doongaloong!";
