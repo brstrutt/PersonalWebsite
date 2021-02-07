763cdfdc52bc932bd54a8ae31ffd24d6f7164e14
isienmai <ben.strutt@hotmail.co.uk>
2021-02-07 14:53:50 +0000

    Remove debug statements. I think it may be working now.

--- ResetDatabaseToCurrentPosts.php
+++ ResetDatabaseToCurrentPosts.php
@@ -1,9 +1,6 @@
 <?php
-echo "Include the database functions!";
 $_SERVER['DOCUMENT_ROOT'] = dirname(__FILE__);
-echo "HMMM" . $_SERVER['DOCUMENT_ROOT'] . "YES";
 include_once($_SERVER['DOCUMENT_ROOT'] . "/DatabaseFunctions.php");
-echo "Start resetting the doongaloong!";
 BS_ResetDatabaseToCurrentPosts();
 echo "Done!";
 ?>
