395874d275790fc6f09b5df686b485d5737970e3
isienmai <ben.strutt@hotmail.co.uk>
2021-02-07 11:06:01 +0000

    Add echoed messages to help debug the cron job not working.

--- ResetDatabaseToCurrentPosts.php
+++ ResetDatabaseToCurrentPosts.php
@@ -1,4 +1,7 @@
 <?php
+  echo "Include the database functions!";
   include_once($_SERVER['DOCUMENT_ROOT'] . "/DatabaseFunctions.php");
+  echo "Start resetting the dingaling!";
   BS_ResetDatabaseToCurrentPosts();
+  echo "Done!";
 ?>
